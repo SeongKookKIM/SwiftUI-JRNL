fdfde79f9ba6ee06c4725c27dc331fafb15d6b1d
SeongKookKIM <ksk8646@naver.com>
2024-05-28 17:07:56 +0900

    Add TapView

---
+++ JRNL-SwiftUI/JournalListView.swift
@@ -0,0 +1,18 @@
+//
+//  JournalListView.swift
+//  JRNL-SwiftUI
+//
+//  Created by SeongKook on 5/28/24.
+//
+
+import SwiftUI
+
+struct JournalListView: View {
+    var body: some View {
+        Text("Jornal List")
+    }
+}
+
+#Preview {
+    JournalListView()
+}
---
+++ JRNL-SwiftUI/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  JRNL-SwiftUI
+//
+//  Created by SeongKook on 5/28/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map View")
+    }
+}
+
+#Preview {
+    MapView()
+}
